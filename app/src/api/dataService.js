9b06d82f1646c4af7df7681497de649bf7eab773
Mateo Noel Rabines <39866695+mateonoel2@users.noreply.github.com>
2023-10-31 14:38:09 -0500

    Delete app directory

--- app/src/api/dataService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = 'http://localhost:8080';
-
-export const fetchGroups = async () => {
-    return axios.get(`${BASE_URL}/groups`)
-}
--- app/src/utils/default-user.js
+++
@@ -1,6 +0,0 @@
-const userInfo = {
-  email: 'sandra@example.com',
-  avatarUrl: 'https://js.devexpress.com/Demos/WidgetsGallery/JSDemos/images/employees/06.png'
-}
-
-export default userInfo;
